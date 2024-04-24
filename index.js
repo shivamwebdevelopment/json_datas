6be11160d0e9f76168d6ab21482ba9aee15e2a0a
Shivam Kumar <41477643+shivamwebdevelopment@users.noreply.github.com>
2024-04-24 10:03:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const jsonData = require("./db.json");
 
+const cors = require("cors");
+
 const port = process.env.PORT || 5000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Manchester United Players");
